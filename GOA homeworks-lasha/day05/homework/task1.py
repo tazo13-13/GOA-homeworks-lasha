aa9a72e8a74745c09ae65d1bf8fd9988a58a31e6
Lasha-30 <lashatertera30@gmail.com>
2025-11-25 23:24:39 +0400

    commit

---
+++ GOA homeworks-lasha/day05/homework/task1.py
@@ -0,0 +1,35 @@
+# 1
+# debugging - ნიშნავს შეცდომების პოვნას და გასწორებას.
+
+
+# 2
+name = 14
+name = (name - 2)
+print(name)
+
+
+# 3
+num1 = "10"
+num2 = "5"
+num3 = (num1 + num2)
+print(num3)
+
+
+# 4
+next1 = 4564 % 4
+print(next1)
+
+next2 = 99768 % 3
+print(next2)
+
+next3 = 16785 % 3
+print(next3)
+
+
+# 5
+name = "saba"
+name = "giorgi"
+print(name) # არ უნდა იყოს Name უნდა იყოს - name
+
+num = 40    # არ უნდა იყოს "40" რადგან სტრინგების გამრავლება სტრინგზე შეუძლებელია ამიტომ უნდა ჩავასწოროდ და დავწეროთ - 40
+print(num * "3")
