2f2932f33d48c6c20bcf282425842d8f981fef01
Lasha-30 <lashatertera30@gmail.com>
2025-11-30 10:44:27 +0400

    commit

---
+++ day09/classwork/classwork.py
@@ -0,0 +1,26 @@
+# 1
+# implicit data conversion - როდესაც ავტომატურად ცვლის
+# explicit - როდესაც ჩვენი ხელით ვცვლით მონაცემთ ტიპს.
+
+
+# 2
+age = str(input("enter uour age : "))
+age = int(input("enter uour age : "))
+age = float(input("enter uour age : "))
+
+
+# 3
+# უბრალოდ გამოიტანს <class float რადგან type გვაქ.
+
+
+# 4
+number = 6.0 
+number > 6
+# false - გამოიტანს
+
+
+# 5
+a = 25
+b = 5
+age = a // b
+print(age)
