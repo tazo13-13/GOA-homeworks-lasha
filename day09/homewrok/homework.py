34fef9089f33eda6a0fce674f75a61adbd879a65
Lasha-30 <lashatertera30@gmail.com>
2025-11-30 10:47:10 +0400

    commit

---
+++ day09/homewrok/homework.py
@@ -0,0 +1,31 @@
+# 1
+# float() - მონაცემი გადაყავშ float ად.
+# str() - მონაცემი გადაყავს სტრინგად.
+# int() - მონაცემი გადაყავს ინტეჯერად.
+
+
+# 2
+age = int(input("enter your age :"))
+final_age = (age + 25)
+print(final_age)
+
+
+# 3
+name = input("enter your name : ")
+print(type(name))
+
+
+# 4
+age = input("enter  your age : ")
+print(str(age))
+
+
+# 5
+num1 = (25>5)
+num2 = (10<5)
+num3 = (15>=5)
+num4 = (5<=5)
+
+
+# 6 
+# sololearn - შევასრულე.
