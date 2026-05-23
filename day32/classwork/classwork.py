c2e3ddb8ef4072503eca3764446c6d4fcdb440d0
Lasha-30 <lashatertera30@gmail.com>
2026-05-23 11:59:44 +0400

    commit

---
+++ day32/classwork/classwork.py
@@ -0,0 +1,13 @@
+# 1
+def join_clone():
+    fruits = ["ვაშლი","ბანანი","ფორთოხალი"]
+    result = ""
+    for i in range(len(fruits)):
+        result += "_"
+        return result
+print(join_clone())
+
+
+
+
+#
